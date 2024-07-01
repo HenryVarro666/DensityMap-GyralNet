4c785d1857212d8630f1f56b2ae1ebbefc40ecca
HenryVarro666 <1504517223@qq.com>
2024-07-01 00:22:50 -0400

    single_subject_GDM_generate

--- gradient_generate.py
+++ gradient_generate.py
@@ -2,7 +2,7 @@
 Author: HenryVarro666 1504517223@qq.com
 Date: 1969-12-31 19:00:00
 LastEditors: HenryVarro666 1504517223@qq.com
-LastEditTime: 2024-06-30 23:55:59
+LastEditTime: 2024-07-01 00:16:40
 FilePath: /DensityMap+GNN/gradient_generate.py
 '''
 import os
@@ -47,31 +47,21 @@ def create_morph_data():
     Returns:
         None
     """
-    # data_dir = "/work/users/j/i/jialec/For_Caochao/HCP_data"
-    # file_list = os.listdir(data_dir)
-    # pbar = tqdm(file_list)
-
     data_dir = "./100206/100206_recon/surf"
-    # file_list = os.listdir(data_dir)
-    pbar = tqdm(data_dir)
-    for file in pbar:
+    file_list = os.listdir(data_dir)
+
+    for file in file_list:
         if '.withGrad.32k_fs_LR.Inner.vtk' not in file:
             continue
-        subject_id = file.split('.')[0]
-        if '.L.' in file:
+        if 'lh' in file:
             hemi = 'lh'
-        elif '.R.' in file:
+        elif 'rh' in file:
             hemi = 'rh'
-
         file_path = os.path.join(data_dir, file)
         surf = pyvista.read(file_path)
         Gradient_Density = surf['gradient_density']
-        # curv = surf['curv']
-        # thickness = surf['thickness']
+        fio.write_morph_data(os.path.join(data_dir, "%s.grad"%(hemi)), Gradient_Density, fnum=327680)
 
-        fio.write_morph_data(os.path.join(data_dir, "%s.%s.grad"%(subject_id, hemi)), Gradient_Density, fnum=327680)
-        # fio.write_morph_data(os.path.join(data_dir, "%s.%s.curv"%(subject_id, hemi)), curv, fnum=327680)
-        # fio.write_morph_data(os.path.join(data_dir, "%s.%s.thickness"%(subject_id, hemi)), thickness, fnum=327680)
     return
 
 if __name__ == "__main__":
