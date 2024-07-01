8d180313e04158644db793849d92c604670430ce
HenryVarro666 <1504517223@qq.com>
2024-07-01 03:49:09 -0400

    GDM_Test

---
+++ GDM_Test/GDM_test.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+###
+ # @Author: HenryVarro666 1504517223@qq.com
+ # @Date: 1969-12-31 19:00:00
+ # @LastEditors: HenryVarro666 1504517223@qq.com
+ # @LastEditTime: 2024-07-01 03:45:36
+ # @FilePath: /DensityMap-GyralNet/GDM_Test/GDM_test.sh
+### 
+
+# Check for the correct number of input arguments
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 cpu_cores"
+    exit 1
+fi
+
+# Read the argument for the number of CPU cores
+cpu_cores=$1
+
+# Function to process each subject folder
+process_folder() {
+    folder=$1
+    cd "$folder" || exit
+    echo "Processing: $folder"
+
+    # Extract the subject ID from the folder name
+    subject_id=$(basename "$folder")
+
+    # Check if the subject ID is a number
+    if ! [[ $subject_id =~ ^[0-9]+$ ]]; then
+        echo "Skipping: $folder is not a valid subject ID"
+        return
+    fi
+
+
+    # Run gyralnet.py
+    python3 /home/lab/Documents/HCP_data/gyralnet.py --root_dir="$PWD" --subject_list_start_id="$subject_id" --subject_list_end_id="$((subject_id + 1))" --input_dir="${subject_id}_recon" 
+
+    echo "Completed processing: $subject_id" 
+}
+
+# Export function for parallel to use
+export -f process_folder
+
+# Get all subject folders in the current directory
+subject_folders=($(find . -maxdepth 1 -type d -name "*" | sed 's|./||'))
+
+# Process each directory using GNU Parallel
+parallel -j $cpu_cores process_folder ::: "${subject_folders[@]}"
+
+echo "All processing complete."
